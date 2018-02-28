7efa985571a9fb5ed204353db052db47fd38c839
Michael Stemen <mstemen@Computer7378.fios-router.home>
2018-02-28 11:05:16 -0500

    attempt to fix mouse

--- keyboards/planck/keymaps/MDS/keymap.c
+++ keyboards/planck/keymaps/MDS/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	{KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
 	{KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
 	{SFT_CAP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SFT_ENT},
-	{KC_LCTL, KC_LALT, KC_LGUI, KC_VOLD, ACTION,  KC_SPC(LT_MC),  KC_SPC,  NUMBER,  KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT}
+	{KC_LCTL, KC_LALT, KC_LGUI, KC_VOLD, ACTION,  LT_MC(KC_SPC),  KC_SPC,  NUMBER,  KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT}
 },
 
 /* Mouse Layer
